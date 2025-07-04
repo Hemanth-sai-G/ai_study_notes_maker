d08b47f5b085e99bd733fdf3e4948462e2229185
Hemanth-sai-G <hemanth.23bce9834@vitapstudent.ac.in>
2025-07-04 14:00:37 +0530

    Delete __init__.py

--- __init__.py
+++
@@ -1,6 +0,0 @@
-# Utils package for Study Notes Maker
-from .file_processor import FileProcessor
-from .ai_generator import AIGenerator
-from .pdf_exporter import PDFExporter
-
-__all__ = ['FileProcessor', 'AIGenerator', 'PDFExporter']
